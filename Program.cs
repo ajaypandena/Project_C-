bc3545d152574e594e4662142288340ac846839c
JDSPORTS\Ajay.Pandena <ajay.pandena@jdplc.com>
2024-05-27 10:09:32 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿namespace ListOfProgram
+{
+
+
+    public class Solution
+    {
+
+        static void Main(string[] args)
+        {
+            //int res = LengthOfLongestSubstring("pwwkew");
+            int res = LengthOfLongestSubstring("12345");
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+
+            int LengthOfLongestSubstring(string s)
+            {
+                int lenth = 0;
+                int i = 0; //left pointer
+                Dictionary<char, int> seen = new();
+                int stringMaxLenth = s.Length;
+                for (int j = i; j < stringMaxLenth; j++)
+                {
+                    char c = s[j];
+                    if (seen.ContainsKey(c) && seen[c] >= i)
+                    {
+                        i = seen[c] + 1;
+                    }
+                    else
+                    {
+                        lenth = Math.Max(lenth, j - i + 1);
+                    }
+                    if (!seen.ContainsKey(c))
+                    {
+                        seen.Add(c, j);
+                    }
+                    else
+                        seen[c] = j;
+                }
+                return lenth;
+            }
+        }
+
+
+    }
+}
+
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //     int res=   LengthOfLongestSubstring("pwwkew");
+    //        Console.WriteLine(res);
+    //        Console.ReadKey();
+    //    }
+
+    //        //public static int LengthOfLongestSubstring(string s)
+    //        //{
+    //        //    string result = string.Empty;
+
+    //        //        if (!result.Contains(s))
+    //        //        {
+    //        //            result += s;
+    //        //        }
+
+    //        //    return result.Length;
+    //        //}
+
+
+
+    //}
